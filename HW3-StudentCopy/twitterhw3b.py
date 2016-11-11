1b668d360fe125de4b4ccb4b220a68c67c9e3325
Jonathan Bracci <jackb1412@gmail.com>
2016-11-11 17:15:22 -0500

    10th commit - twitter codes

--- HW3-StudentCopy/twitterhw3b.py
+++ HW3-StudentCopy/twitterhw3b.py
@@ -17,10 +17,11 @@ import requests
 import requests_oauthlib
 from textblob import TextBlob
 
-access_token = ""
-access_token_secret = ""
-consumer_key = ""
-consumer_secret = ""
+access_token = "331897710-t8nnZuAh1ofe0lCUSfKBQNlqBOMHgGUooIy5UN4x"
+access_token_secret = "FNc3y1c8FgzFezvAf0ml5ofVEAE3JE7f1zSsyq7W8OeFE"
+consumer_key = "ixWyhyZ1BDfiy2qgSLGjrA57y"
+consumer_secret = "m1rjKpFhfXhRBzET9dEPIciaY4HHHQAj5urznBlm7yY7273BYk"
+
 
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
